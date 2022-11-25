a3f8a4d9fed5ea7975e6fd2c58cd0abf8705a027
msaimraz <msraza.02@gmail.com>
2022-11-25 14:00:57 +0500

    smilecare

--- js/custom.js
+++ js/custom.js
@@ -1,15 +1,27 @@
 $(document).ready(function() {
-    $('.autoplay').slick({
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-    });
+    let navScroll = 0;
+    $(document).scroll(function() {
+        navScroll = $(this).scrollTop();
+        if (navScroll > 10) {
+            $("nav").css({
+                "background-color": "#482AFD24",
+                "backdrop-filter": "saturate(180%) blur(20px)"
+            });
+
+        } else {
+            $("nav").css({
+                "background-color": "none",
+                "backdrop-filter": "none"
+            });
+
+        }
+    })
 });
-AOS.init();
+
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 3,
     autoplay: true,
     spaceBetween: 20,
 
 });
+AOS.init();
